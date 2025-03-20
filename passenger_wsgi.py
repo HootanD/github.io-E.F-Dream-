fca454a2a5f549291917e01e9fcd6b1ad68b0dc7
Hootan Dehbonei <hootan@everlastingfairytale.com.au>
2025-03-20 09:33:59 +0800

    EqualTo allows rounding errors (code and unit test).

---
+++ passenger_wsgi.py
@@ -0,0 +1,9 @@
+import imp
+import os
+import sys
+
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+wsgi = imp.load_source('wsgi', 'https://github.com/HootanD/github.io-E.F-Dream-.git')
+application = wsgi.https://github.com/HootanD/github.io-E.F-Dream-.git
